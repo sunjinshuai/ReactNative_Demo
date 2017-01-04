191dccb5c74210e97b50a242389392dc816b2afe
sunjinshuai <s_mike@163.com>
2017-01-04 12:02:49 +0800

    demo
    hello world

--- index.ios.js
+++ index.ios.js
@@ -4,33 +4,36 @@
  * @flow
  */
 
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import {
-   AppRegistry,
-   StyleSheet,
-   Navigator
+    AppRegistry,
+    StyleSheet,
+    Navigator,
+    Image,
+    Text,
+    View
 } from 'react-native';
 
 import FirstPageComponent from './View/Day1/FirstPageComponent';
 
 export default class ReactNative_Demo extends React.Component {
-   render() {
+    render() {
 
-       let defaultName = 'FirstPageName';
-       let defaultComponent = FirstPageComponent;
-
-       return (
-         <Navigator
-               initialRoute={{ name: defaultName, component: defaultComponent }}
-               configureScene={(route) => {
-               return Navigator.SceneConfigs.FloatFromRight;
-           }}
-           renderScene={(route, navigator) => {
-               let Component = route.component;
-               return <Component {...route.params} navigator={navigator} />
-         }}/>
-       );
-     }
+        return (
+            <View style={styles.container}>
+                <Text>Hello World!</Text>
+            </View>
+        );
+    }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#DEB887',
+    },
+});
 
 AppRegistry.registerComponent('ReactNative_Demo', () => ReactNative_Demo);
