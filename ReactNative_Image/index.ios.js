ede6c86d1d0e0e61fb118dbc32b4542fc5b1bc38
sunjinshuai <s_mike@163.com>
2017-01-26 22:57:22 +0800

    ReactNative之Image
    ReactNative之Image

---
+++ ReactNative_Image/index.ios.js
@@ -0,0 +1,44 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, {Component} from 'react';
+import {
+    AppRegistry,
+    StyleSheet,
+    Text,
+    View,
+    Image
+} from 'react-native';
+
+export default class ReactNative_Image extends Component {
+    render() {
+        return (
+            <Image source={{uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'}}
+                   style={{width: 193, height: 110}}/>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF',
+    },
+    welcome: {
+        fontSize: 20,
+        textAlign: 'center',
+        margin: 10,
+    },
+    instructions: {
+        textAlign: 'center',
+        color: '#333333',
+        marginBottom: 5,
+    },
+});
+
+AppRegistry.registerComponent('ReactNative_Image', () => ReactNative_Image);
