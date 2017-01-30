fb89cff3966158178053ceb325cec8fb4ce8aec3
sunjinshuai <s_mike@163.com>
2017-01-30 13:35:59 +0800

    ReactNative组件之Text

--- ReactNative_Text/index.ios.js
+++ ReactNative_Text/index.ios.js
@@ -15,26 +15,38 @@ import {
 export default class ReactNative_Text extends Component {
   render() {
       return (
-          <Text style={styles.outerText}>I am learning React Native!
-            <Text style={styles.innerText}>Please study hard!
-            </Text>
-          </Text>
+          <View style={styles.container}>
+              <Text style={styles.textViewStyle} numberOfLines={6}>
+                  演示的文字,演示的文字,演示的文字,演示的文字,演示的文字,演示的文字,演示的文字,演示的文字,演示的文字,演示的文字,演示的文字,演示的文字,演示的文字,演示的文字,演示的文字,演示的文字,演示的文字,演示的文字,演示的文字
+              </Text>
+          </View>
       );
   }
 }
 
 const styles = StyleSheet.create({
-    outerText:{
-        margin:40,
-        textAlign:'center',
-        color:'red',
-        fontSize:28,
-        fontFamily:'Cochin'
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF',
     },
-    innerText: {
-        color:'green',
-        fontWeight:'bold',
-    },
+    textViewStyle:{
+        color:'red',  //字体颜色
+        fontSize:23,  //字体大小
+        fontStyle:'normal',   //字体风格,枚举值,normal,bold
+        fontWeight: 'bold',     //字体粗细权重,枚举值,normal,bold,100,200,300,400,500,600,700,800,900
+        textShadowOffset:{width:3, height:4}, //阴影效果
+        textShadowRadius: 2, //阴影效果圆角
+        textShadowColor:'yellow',//阴影效果颜色
+        letterSpacing:3 , //字符间距
+        lineHeight:80 , //行高
+        textAlign:'center', //文字对齐方式,枚举值:auto left right center justify
+        textDecorationLine:'underline', //横线位置,枚举值,none,underline,line-through,underline line-through
+        textDecorationStyle:'solid', //线的风格,solid double dotted dashed
+        textDecorationColor:'red', //线的颜色
+        writingDirection:'ltr' //文本方向.枚举值,auto,ltr,rtl
+    }
 });
 
 AppRegistry.registerComponent('ReactNative_Text', () => ReactNative_Text);
